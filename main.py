c743f9f16c7bd13d0be498bb76dbd4a0aa3ab0b3
bVd <vandenabeele.bastiaan@gmail.com>
2018-08-30 20:04:41 +0200

    rough plan

---
+++ main.py
@@ -0,0 +1,21 @@
+
+
+# read the csv files
+
+# process into easy manageable objects
+
+# find combinations objects
+
+#    combinations source destination
+
+#    check time diference 1-4 hours
+
+#    maximun allowed bags
+
+#    call price (if possible get flight price from api)
+#        flights-browse-prices
+#           https: // skyscanner.github.io/slate /?_ga = 1.104705984.172843296.1446781555
+
+# ouput data in a readable form -> json
+
+# figure out what stdio stdout and stderr are
