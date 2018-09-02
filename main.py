937dc888247e4b0ee77ed445e6f8df63be6b0b91
bVd <vandenabeele.bastiaan@gmail.com>
2018-09-02 14:45:19 +0200

    small refactor and extra doc

--- main.py
+++ main.py
@@ -5,9 +5,12 @@ from read_csv import read_in_csv
 from flight import Flight
 from combinations import FlightCombinations
 
+# read in csv to python objects
 input_data_sorted = read_in_csv(sys.stdin)
-valid_flight_combinations = list()
+
+# find all the valid flight combinations
 second_data = input_data_sorted.copy()
+valid_flight_combinations = list()
 
 for first_flight_info in input_data_sorted:
     second_data.pop(0)
@@ -19,10 +22,10 @@ for first_flight_info in input_data_sorted:
         if flight_combi.is_valid():
             valid_flight_combinations.append(flight_combi)
 
+# call price for bags 0 , 1 2
 max_bags = 2
 combinations = list()
 for bags in range(max_bags+1):
-    # combinations['combinationsset'] = {'combinations by bag': list(), 'bags': bags}
     for valid_combination in valid_flight_combinations:
         if valid_combination.max_allowed_bags() >= bags:
             price = valid_combination.total_price(total_bags=bags)
